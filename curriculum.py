33d5286116657bd5b786d563ef8976b604eec528
raidel <raidel@uct.geocuba.cu>
2023-01-23 12:42:50 -0500

    update 1

--- curriculum.py
+++ curriculum.py
@@ -1,9 +1,9 @@
 import shutil
 
-path_english = r"/home/raidel/Documentos/curriculum/latex/inglés/resume.pdf"
-file_english = r"/home/raidel/Documentos/curriculum/latex/CV_EN_Raidel_Nápoles.pdf"
-path_spanish = r"/home/raidel/Documentos/curriculum/latex/español/resume.pdf"
-file_spanish = r"/home/raidel/Documentos/curriculum/latex/CV_ES_Raidel_Nápoles.pdf"
+path_english = r"./inglés/resume.pdf"
+file_english = r"./CV_EN_Raidel_Nápoles.pdf"
+path_spanish = r"./español/resume.pdf"
+file_spanish = r"./CV_ES_Raidel_Nápoles.pdf"
 
 shutil.copyfile(path_english, file_english)
 shutil.copyfile(path_spanish, file_spanish)
