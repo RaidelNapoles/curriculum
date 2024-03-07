680b8c0318434984b8f6625ec327d4b53af35722
Raidel <raidelnapoles95@gmail.com>
2024-03-07 16:34:40 -0500

    update for working in all OS

--- curriculum.py
+++ curriculum.py
@@ -1,9 +1,12 @@
 import shutil
+import os
 
-path_english = r"./inglés/resume.pdf"
-file_english = r"./CV_EN_Raidel_Nápoles.pdf"
-path_spanish = r"./español/resume.pdf"
-file_spanish = r"./CV_ES_Raidel_Nápoles.pdf"
+current_directory = os.path.dirname(__file__)
+
+path_english = os.path.join(current_directory, "inglés", "resume.pdf")
+file_english = os.path.join(current_directory, "CV_EN_Raidel_Nápoles.pdf")
+path_spanish = os.path.join(current_directory, "español", "resume.pdf")
+file_spanish = os.path.join(current_directory, "CV_ES_Raidel_Nápoles.pdf")
 
 shutil.copyfile(path_english, file_english)
 shutil.copyfile(path_spanish, file_spanish)
